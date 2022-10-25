d1ca2741374807cbf3a124e899afda256bdd6566
meredithdean <meredithdvhamilton@gmail.com>
2022-10-25 15:59:57 +0100

    Add ideasRouter code

--- server/ideas.js
+++ server/ideas.js
@@ -7,3 +7,44 @@ const {
     updateInstanceInDatabase,
     deleteFromDatabasebyId,
   } = require('./db');
+
+  ideasRouter.param('id', (req, res, next, id) => {
+    const idea = getFromDatabaseById('ideas', id);
+    if (idea) {
+        req.idea = idea;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});  
+
+ideasRouter.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('ideas'));
+});
+
+ideasRouter.post('/', (req, res, next) => {
+    const newIdea = addToDatabase('ideas', req.body);
+    res.status(201).send(newIdea);
+});
+
+ideasRouter.get('id', (req, res, next) => {
+    res.send(req.idea)
+});
+
+ideasRouter.put('/:id', (req, res, next) => {
+    let updatedIdeaInstance = updateInstanceInDatabase('ideas', req.body)
+    res.send(updatedIdeaInstance);
+});
+
+ideasRouter.delete('/:id', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('ideas', req.params.id);
+    if (deleted) {
+        res.status(204);
+    } else {
+        res.status(500);
+    }
+    res.send();
+});
+
+
+module.exports = ideasRouter;
