99d6670a37c5e0048ef598818373838436a3a9a6
meredithdean <meredithdvhamilton@gmail.com>
2022-10-25 17:13:39 +0100

    Import and mount routers in apiRouter

--- server/api.js
+++ server/api.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+// import routers from files
+const minionsRouter = require('./minions');
+const ideasRouter = require('./ideas');
+const meetingsRouter = require('./ideas');
 
-
-
-
-
-
+// mount routers for use
+apiRouter.use('/minions', minionsRouter);
+apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
 
 module.exports = apiRouter;
