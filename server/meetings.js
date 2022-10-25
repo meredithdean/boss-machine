3682ee61160e046cb571fb9bdf21baf7f6196170
meredithdean <meredithdvhamilton@gmail.com>
2022-10-25 16:18:44 +0100

    Add meetingsRouter code

---
+++ server/meetings.js
@@ -0,0 +1,25 @@
+const meetingsRouter = require('express').Router();
+
+const { 
+    addToDatabase,
+    getAllFromDatabase,
+    deleteAllFromDatabase,
+    createMeeting
+  } = require('./db');
+
+  meetingsRouter.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('meetings'));
+});
+
+meetingsRouter.post('/', (req, res, next) => {
+    const newMeeting = addToDatabase('meetings', createMeeting());
+    res.status(201).send(newMeeting);
+});
+
+meetingsRouter.delete('/', (req, res, next) => {
+    const deleted = deleteAllFromDatabase('meetings');
+    res.status(204).send();
+});
+
+
+module.exports = meetingsRouter;
